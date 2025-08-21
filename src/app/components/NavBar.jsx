00dbc2d71de0606b18e155917bd57f87d4a14fa2
julkarzunayed <zunayedjulkar@gmail.com>
2025-08-21 11:08:37 +0600

    first commit

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav className="">
+            <ul className='flex justify-center gap-5 p-2 border'>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar />
+        <main className="min-h-[80vh]">
+          {children}
+        </main>
+        <footer>
+          this is footer
+        </footer>
       </body>
     </html>
   );
